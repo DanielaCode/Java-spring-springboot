9c16092c0adcd36d5e890ffcfc73c7452dd514c6
Daniela <danielagp96@gmail.com>
2023-05-31 22:53:57 -0600

    object creation + wiring dependencies we are injecting an object to game runner  but what happens if we have thousand clases, it is difficult to manage so spring have a mechanism to do it. -here we are currently creating and managing the objects and these are adding to the JVM java virtual machine -better approach use spring to do it (next step)

--- src/main/java/com/daniCode/AppGameRunner/AppGamingBasicJava.java
+++ src/main/java/com/daniCode/AppGameRunner/AppGamingBasicJava.java
@@ -7,9 +7,9 @@ public class AppGamingBasicJava {
     public static void main(String[] args){
         //GammingConsole game = new MarioGame();
         //GammingConsole game = new SuperContraGame();
-        GammingConsole game = new PacmanGame();
+        GammingConsole game = new PacmanGame();//object creation
 
-        GameRunner gr = new GameRunner(game);
+        GameRunner gr = new GameRunner(game);//object creation + wiring dependencies we are injecting an object to game runner 
         gr.run();
     }
 
